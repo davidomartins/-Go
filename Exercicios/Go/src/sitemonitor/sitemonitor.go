c72d1189a0fbaad09d059dc9ebe8c774e516323b
David Martins <davidomartins@gmail.com>
2022-08-24 00:06:02 -0400

    Monitoramento com net/http implementado

--- Exercicios/Go/src/sitemonitor/sitemonitor.go
+++ Exercicios/Go/src/sitemonitor/sitemonitor.go
@@ -2,37 +2,57 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
+	var comando int
+	for {
+		exibeMenu()
+		lerComando(&comando)
+		executaComando(&comando)
+	}
+}
+
+func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do programa")
+}
 
-	comando := 0
-	fmt.Scan(&comando)
+func lerComando(comandoLido *int) {
+	fmt.Scan(comandoLido)
+}
 
-	fmt.Println("O comando selecionado foi", comando)
+func executaComando(comandoLido *int) {
+	var url string
 
-	/* 	if comando == 1 {
-	   		fmt.Println("Monitorando...")
-	   	} else if comando == 2 {
-	   		fmt.Println("Exibindo os logs...")
-	   	} else if comando == 3 {
-	   		fmt.Println("Saindo do programa...")
-	   	} else {
-	   		fmt.Println("Comando desconhecido")
-	   	} */
+	fmt.Println("O comando selecionado foi", *comandoLido)
 
-	switch comando {
+	switch *comandoLido {
 	case 1:
-		fmt.Println("Monitorando...")
+		fmt.Println("Qual site você quer monitorar?")
+		fmt.Scan(&url)
+		fmt.Println("Monitorando", url, "...")
+		statCode := monitoraSite(url)
+		if statCode == 200 {
+			fmt.Println("Site", url, "carregado com sucesso!")
+		} else {
+			fmt.Println("Site", url, "com problema, status:", statCode)
+		}
 	case 2:
 		fmt.Println("Exibindo os logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Comando desconhecido")
+		os.Exit(-1)
 	}
+}
 
+func monitoraSite(url string) int {
+	resp, _ := http.Get(url)
+	return resp.StatusCode
 }
