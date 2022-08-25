a9b55373e87993229361b298b64a8e47b497b22d
David Martins <davidomartins@gmail.com>
2022-08-25 16:20:00 -0400

    Utilizado slice para criar uma lista de sites

--- Exercicios/Go/src/sitemonitor/sitemonitor.go
+++ Exercicios/Go/src/sitemonitor/sitemonitor.go
@@ -11,7 +11,7 @@ func main() {
 	for {
 		exibeMenu()
 		lerComando(&comando)
-		executaComando(&comando)
+		executaComando(comando)
 	}
 }
 
@@ -25,22 +25,10 @@ func lerComando(comandoLido *int) {
 	fmt.Scan(comandoLido)
 }
 
-func executaComando(comandoLido *int) {
-	var url string
-
-	fmt.Println("O comando selecionado foi", *comandoLido)
-
-	switch *comandoLido {
+func executaComando(comandoLido int) {
+	switch comandoLido {
 	case 1:
-		fmt.Println("Qual site você quer monitorar?")
-		fmt.Scan(&url)
-		fmt.Println("Monitorando", url, "...")
-		statCode := monitoraSite(url)
-		if statCode == 200 {
-			fmt.Println("Site", url, "carregado com sucesso!")
-		} else {
-			fmt.Println("Site", url, "com problema, status:", statCode)
-		}
+		monitorarSites()
 	case 2:
 		fmt.Println("Exibindo os logs...")
 	case 0:
@@ -52,7 +40,26 @@ func executaComando(comandoLido *int) {
 	}
 }
 
-func monitoraSite(url string) int {
+func statusCodeDoSite(url string) int {
 	resp, _ := http.Get(url)
 	return resp.StatusCode
+}
+
+func listaDeSites() []string {
+	sites := []string{"https://www.alura.com.br", "https://www.caelum.com.br/", "https://www.caelum.com.br/xpto"}
+	return sites
+}
+
+func monitorarSites() {
+	listaDeSite := listaDeSites()
+	for i := 0; i < len(listaDeSite); i++ {
+		url := listaDeSite[i]
+		fmt.Println("Monitorando", url, "...")
+		statCode := statusCodeDoSite(url)
+		if statCode == 200 {
+			fmt.Println("Site", url, "carregado com sucesso!")
+		} else {
+			fmt.Println("Site", url, "com problema, status:", statCode)
+		}
+	}
 }
